9df9ab4b51fb94a5f314a705a4c6353755a30a8d
ITANéA <frederic.blanc@itanea.fr>
2020-05-10 16:13:46 +0200

    first implementation of form, adding route to send data

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,41 +64,53 @@
             margin-bottom: 30px;
         }
     </style>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
-    <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-            <a href="{{ url('/home') }}">Home</a>
-            @else
-            <a href="{{ route('login') }}">Login</a>
-
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a>
-            @endif
-            @endauth
-        </div>
-        @endif
-
-        <div class="content">
-            <div class="title m-b-md">
-                Laravel
-            </div>
-
-            <div class="links">
-                <a href="https://laravel.com/docs">Docs</a>
-                <a href="https://laracasts.com">Laracasts</a>
-                <a href="https://laravel-news.com">News</a>
-                <a href="https://blog.laravel.com">Blog</a>
-                <a href="https://nova.laravel.com">Nova</a>
-                <a href="https://forge.laravel.com">Forge</a>
-                <a href="https://vapor.laravel.com">Vapor</a>
-                <a href="https://github.com/laravel/laravel">GitHub</a>
+    <div class="container flex-center position-ref full-height">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <img class="card-img-top"
+                        src="https://via.placeholder.com/728x90.png/01f/fff?text=Inscrivez vous à notre newsletter"
+                        alt="">
+                    <div class="card-body">
+                        <div class="form-group">
+                            <form action="{{ route('store') }}" method="post">
+                                @csrf
+                                <label for="newsletter-email">Merci de saisir votre email</label>
+                                <input type="email" class="form-control" name="newsletter-email" id="newsletter-email"
+                                    aria-describedby="emailHelpId" placeholder="john.doe@mail-exemple.fr">
+                                <small id="emailHelpId" class="form-text text-muted">Vous pouvez nous confier votre
+                                    email,
+                                    promis aucun spam et il sera protégé sur nos serveurs comme un trésor !</small>
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-primary my-4 btn-lg">Envoyer moi votre
+                                        newsletter</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
